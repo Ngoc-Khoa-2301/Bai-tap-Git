12cef89decb609254fd176c95a691498b6dac649
wannacryvn048 <vieejtyl.capcap@gmail.com>
2023-10-21 17:28:28 +0700

    thuong

--- Source.cpp
+++ Source.cpp
@@ -4,11 +4,18 @@ using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
-float thuong(int a, int b);
+float thuong(int a, int b){
+	return a/b;
+}
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+int a,b;
+cout<<"nhap a:"
+cin>>a;
+cout<<"nhap b"
+cin>>b;
+cout<<a<<"/"<<b<<"="<<thuong(a,b)<<enld;
 	system("pause");
 	return 0;
 }
